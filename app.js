bf59689952a4c7cca7c4730723209d67f66255fd
kanleion <manu12694@gmail.com>
2022-05-10 22:12:08 -0400

    cambios con más validaciones

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ new Vue({
 
     computed: {
         totalTime() {
+
+            //validando que el array este vacio o que su propiedad esta vacia
+            if (!this.courses.length) {
+                return 0;
+            }
+
             let sum = 0;
             for (let i = 0; i < this.courses.length; i++) {
                 sum += parseInt(this.courses[i].time);
@@ -22,6 +28,9 @@ new Vue({
 
     methods: {
         addCourse() {
+            if (!this.title || !this.time) {
+                return
+            }
             this.courses.push({ name: this.title, time: this.time });
             this.title = "";
             this.time = 0;
